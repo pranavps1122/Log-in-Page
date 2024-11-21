a4d0a910904fd5e10590e5ea759d5b598b4c1a6e
pranavps1122 <pranavps01@gmail.com>
2024-11-21 18:50:43 +0530

    restructured project files

--- server.js
+++ server.js
@@ -1,48 +1,75 @@
 const express =require('express');
 const path=require('path')
-const bodyParser = require('body-parser');
 const session=require('express-session')
-const{v4:uuidv4}=require('uuid')
 const nocache = require('nocache')
-const router=require('./router');
-
 
 const app=express();
-const Port=process.env.Port||3003;
+
+const PORT = 3003
+
 app.set('view engine','ejs')
 
-
-
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use(nocache());
 
 
 app.use('/static',express.static(path.join(__dirname,'public')))
 app.use('/images',express.static(path.join(__dirname,'public/images')))
+
 app.use(session({
-    secret:uuidv4(),
+    secret:'123456',
     resave:false,
     saveUninitialized:true
 }))
 
-app.use('/route',router);
+const credential = {
+    email: "user@gmail.com",
+    password: "user123"
+};
 
+app.post('/login',(req,res)=>{
+    if(req.body.email==credential.email&&req.body.password==credential.password){
+        req.session.user=req.body.email;
+        res.redirect('/dashboard');
+    }else{
+        res.render('login',{title:"Express" ,alert:"Invalid username or Password"})
+    }
+})
+
+app.get('/',(req,res) => {
+    if(req.session.user){
+        res.redirect('/dashboard');
+    }else{
+        res.render('login')
+    }
+})
+
+
+app.get('/dashboard',(req,res)=>{
+    if(req.session.user){
+        res.render("dashboard",{user:req.session.user})
+    }else{
+        res.redirect('/')
+    }
+})
 
 
 //Home Route..
 app.get('/',(req,res)=>{
-
     if(req.session.user){
-        
         res.render('dashboard',{user:req.session.user})
     }else{
-       
-        res.render('base',{title:"Login System"})
-    }
-   
+        res.render('login',{title:"Login System"})
+    } 
 })
-app.listen(Port,()=>{
-    console.log(`Server Is Runs On http://localhost:3003`)
+
+app.get('/logout', (req, res) => {  
+    req.session.destroy();
+        res.redirect("/")
+    });
+
+
+app.listen(PORT,()=>{
+    console.log(`Server Is Runs On http://localhost:${PORT}`)
 })
